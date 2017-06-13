102effd7bc78d73035d585379326d41012240eec
markv <mark.vandenbos@gmail.com>
2017-06-13 10:54:39 +1000

    rework installer

--- install.sh
+++ install.sh
@@ -1,19 +1,48 @@
 #!/usr/bin/env bash
 
+
+DOT_SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export DOT_SRC
+
+
 DOTFILES=()
-DOTFILES+=('.vimrc')
-DOTFILES+=('.bash_aliases')
-DOTFILES+=('.bash_functions')
-DOTFILES+=('.bash_logout')
-DOTFILES+=('.bash_profile')
-DOTFILES+=('.bashrc')
-DOTFILES+=('.tmux.conf')
-DOTFILES+=('.bash_prompt')
+# format: "SOURCE|DESTINATION"
+DOTFILES+=("${DOT_SRC}/.vimrc|${HOME}/.vimrc")
+DOTFILES+=("${DOT_SRC}/.bash_aliases|${HOME}/.bash_aliases")
+DOTFILES+=("${DOT_SRC}/.bash_functions|${HOME}/.bash_functions")
+DOTFILES+=("${DOT_SRC}/.bash_logout|${HOME}/.bash_logout")
+DOTFILES+=("${DOT_SRC}/.bash_profile|${HOME}/.bash_profile")
+DOTFILES+=("${DOT_SRC}/.bashrc|${HOME}/.bashrc")
+DOTFILES+=("${DOT_SRC}/.tmux.conf|${HOME}/.tmux.conf")
+DOTFILES+=("${DOT_SRC}/.bash_prompt|${HOME}/.bash_prompt")
 
 for DOTFILE in "${DOTFILES[@]}";
 do
-  ln -sv ~/dotfiles/${DOTFILE} ~
+
+  # split var into SRC/DST
+  SRC=${DOTFILE%%|*}
+  DST=${DOTFILE##*|}
+
+  # create symlink if it doesnt exist
+  if [ -L "${DST}" ]; 
+  then
+    echo "symlink exists: ${DST}"
+  else
+    ln -sv "${SRC}" "${DST}"
+  fi
 done
 
-# make vimtmp
-mkdir -v ~/.vimtmp
+
+DIRECTORIES=()
+DIRECTORIES+=("${HOME}/.vimtmp")
+
+# create directories if they dont exist
+for DIRECTORY in "${DIRECTORIES[@]}";
+do
+  if [ -d ${DIRECTORY} ]; 
+  then
+    echo "directory exists: ${DIRECTORY}"
+  else 
+    mkdir -v ${DIRECTORY}
+  fi
+done
